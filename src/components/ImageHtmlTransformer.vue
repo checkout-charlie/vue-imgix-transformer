36068861959ddef2c5f49603864f85fa9e0f4f0a
Emily Wu <embrilliant@hotmail.com>
2018-12-17 00:50:01 +0100

    remove unused functionality

--- src/components/ImageHtmlTransformer.vue
+++ src/components/ImageHtmlTransformer.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <img
-      v-if="imgUrl"
-      :src="transformedUrl"
-      :alt="alt"
-    >
     <!-- eslint-disable-next-line -->
     <div v-if="html" v-html="transformedHtml"></div>
   </div>
@@ -14,47 +9,25 @@
 export default {
   name: 'ImageHtmlTransformer',
   props: {
-    imgUrl: {
-      default: null,
-      type: String
-    },
     html: {
       default: null,
-      type: String
-    },
-    options: {
-      default: null,
-      type: Object
-    },
-    alt: {
-      default: '',
       type: String
     }
   },
   data() {
     return {
-      transformedHtml: '',
-      transformedUrl: ''
+      transformedHtml: ''
     }
   },
   mounted() {
     if (this.html) {
       this.transformHtml()
     }
-    if (this.imgUrl) {
-      this.transformUrl()
-    }
   },
   methods: {
     transformHtml() {
       this.transformedHtml = this.$imgTransformer.getTransformedHtml(
         this.html,
-        this.options
-      )
-    },
-    transformUrl() {
-      this.transformedUrl = this.$imgTransformer.getTransformedUrl(
-        this.imgUrl,
         this.options
       )
     }
