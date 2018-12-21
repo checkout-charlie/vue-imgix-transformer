4f492010c0348806a1814fdbcba6c89cffd2e34a
EmilyWu <emily.wu@sparwelt.de>
2018-12-21 11:38:45 +0100

    add lazysizes package and lazy compoment draft

---
+++ src/components/ImageLazyloader.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src srcset data-srcset data-sizes class="lazyload">
+</template>
+<script>
+  export default {
+    name: 'ImageLazyloader',
+    props: ['src', 'srcset', 'dataSrcset', 'dataSizes', 'cssClasses'],
+    data() {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
